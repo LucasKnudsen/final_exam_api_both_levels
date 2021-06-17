acd8418816639e6a7181916abf3256c8ef485e0a
lucas <laksteelhouse@gmail.com>
2021-06-17 11:35:42 +0200

    Writes happy path request spec for creating comments

---
+++ spec/requests/api/comment/user_can_create_comment_spec.rb
@@ -0,0 +1,28 @@
+RSspec.describe 'POST /api/articles/:id/comments', type: :request do
+  let(:article) { create(:article) }
+
+  describe 'Successfully' do
+    before do
+      post "/api/articles/#{article.id}/comments", 
+        params: {
+          body: 'I truly like this article!'
+        }
+    end
+
+    it 'is expected to return a 201 status code' do
+      expect(response).to have_http_status 201
+    end
+
+    it 'is expected to return a success message' do
+      expect(response_json['message']).to eq 'Successfully posted your comment'
+    end
+
+    it 'is expected to have created a new comment' do
+      expect(Comment.all.count).to eq 1
+    end
+
+    it 'is expected to attach the comment to an article' do
+      expect(article.comments.first['body']).to eq 'I truly like this article!'
+    end
+  end
+end
